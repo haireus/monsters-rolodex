fe36e9670a5f722565534a98a47979491b057d11
haireus <dinhvanhai07071997@gmail.com>
2020-06-09 13:37:55 +0700

    Updates

---
+++ precache-manifest.6f06fd4e520e40281244394eccb26257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce76d96f6a20478263dad0960d467a2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "16c5c06ff66ca22aeeee",
+    "url": "/monsters-rolodex/static/css/main.4de3f4e7.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c5c06ff66ca22aeeee",
+    "url": "/monsters-rolodex/static/js/main.68c2cdc8.chunk.js"
+  },
+  {
+    "revision": "96e4dc8f64b3356b837b",
+    "url": "/monsters-rolodex/static/js/runtime-main.6b89a6eb.js"
+  }
+]);
